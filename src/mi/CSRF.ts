96f3caed378f4db9200f4697dd8b6b930da22ed0
John Hagler <haglerjo@gmail.com>
2025-03-19 10:49:58 -0700

    chg

--- src/mi/CSRF.ts
+++ src/mi/CSRF.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
+import axios, { AxiosHeaders, AxiosRequestConfig } from 'axios';
 
 /**
  * Manages Cross Site Request Forgery (CSRF) tokens used when making interal calls to M3 resources
@@ -9,7 +9,7 @@ export class CSRF {
   /**
    * Retrieves a CSRF token from the M3 engine
    *
-   * Is is rare that a token will need to be manually retieved and used.  The provided function {@link fetch} handles 
+   * Is is rare that a token will need to be manually retieved and used.  The provided function {@link fetch} handles
    * retrieving and using a the CSRF token to make requests.
    *
    * @returns CSRF token
@@ -21,22 +21,21 @@ export class CSRF {
    * ```
    */
   public async getToken() {
-    
-    const resp = await axios("/foundation-rest/csrf");
+    const resp = await axios('/foundation-rest/csrf');
     const token = resp.data;
-    console.log('retrieving csrf token', token)
+    console.log('retrieving csrf token', token);
     return token;
   }
 
   /**
    * Executes a browser [fetch](https://developer.mozilla.org/docs/Web/API/Fetch_API/Using_Fetch) request and automatically provides a valid CSRF token.
-   * 
+   *
    * Additional fetch [options](https://developer.mozilla.org/en-US/docs/Web/API/fetch) can be provided if necesary.
-   * 
-   * @param url 
-   * @param options 
+   *
+   * @param url
+   * @param options
    * @returns fetch response object
-   * 
+   *
    * @example
    * ```ts
    * const csrf = new CSRF()
@@ -51,18 +50,35 @@ export class CSRF {
    * ```
    */
   public async fetch(url: string, options: AxiosRequestConfig) {
-    console.log('fetching with csrf', url)
+    console.log('fetching with csrf', url);
     if (!this.csrfToken) {
       this.csrfToken = await this.getToken();
     }
-    this.addToken(options);
-    let resp = await axios(url, options);
+    const headers: any = options.headers?.raw ?? {};
+
+    let resp = await axios(url, {
+      method: options.method,
+      data: options.data,
+      headers: {
+        'fnd-csrf-token': this.csrfToken,
+        Accept: headers['Accept'],
+        'Content-Type': headers['Content-Type'],
+      },
+    });
     if (resp.status == 403) {
-      const json = resp.data
-      if (json?.message == "CSRF_TOKEN_EXPIRED") {
+      const json = resp.data;
+      if (json?.message == 'CSRF_TOKEN_EXPIRED') {
         this.csrfToken = await this.getToken();
-        this.addToken(options);
-        resp = await axios(url, options);
+        console.log('refreshed token', this.csrfToken);
+        resp = await axios(url, {
+          method: options.method,
+          data: options.data,
+          headers: {
+            'fnd-csrf-token': this.csrfToken,
+            Accept: headers['Accept'],
+            'Content-Type': headers['Content-Type'],
+          },
+        });
       }
     }
 
@@ -71,9 +87,9 @@ export class CSRF {
 
   protected addToken(options: AxiosRequestConfig) {
     if (this.csrfToken) {
-      const headers = new AxiosHeaders(options.headers?.raw)
-      headers.set('fnd-csrf-token', this.csrfToken)
-      options.headers = headers
+      const headers = new AxiosHeaders(options.headers?.raw);
+      headers.set('fnd-csrf-token', this.csrfToken);
+      options.headers = headers;
     }
   }
 }
